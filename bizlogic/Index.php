99eb46182b8feca2d5659085ae92de3512e68073
Nirojan21 <nirojan.yoga@gmail.com>
2022-07-19 14:28:27 +0530

    done

--- bizlogic/Index.php
+++ bizlogic/Index.php
@@ -1,1 +1,32 @@
 <?php
+
+include "db.php";
+
+$db = new DbConnect();
+$conn = $db->connect();
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case 'POST':
+        $user = json_decode(file_get_contents('php://input'));
+        $sql = "SELECT * FROM Exam WHERE email LIKE '$user->email' AND password LIKE '$user->password' ";
+        $stmt = $conn->prepare($sql);
+        if ($stmt->execute()) {
+            $data = ['status' => 1, 'message' => ""];
+        } else {
+            $data = ['status' => 0, 'message' => "Failed to login"];
+        }
+        echo json_encode($data);
+        break;
+    case 'GET':
+        $user = json_decode(file_get_contents('php://input'));
+        $sql = "SELECT * FROM Exam WHERE id LIKE '$user->id' ";
+        $stmt = $conn->prepare($sql);
+        if ($stmt->execute()) {
+            $data = ['status' => 1, 'message' => ""];
+        } else {
+            $data = ['status' => 0, 'message' => "Failed to fectch data"];
+        }
+        break;
+}
